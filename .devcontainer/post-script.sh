d1c0260373e315a90f0a0360007329f3500d2e43
Paul Glavich <paulglavich@microsoft.com>
2025-08-01 05:40:22 +0000

    Added more robust indexing script

--- .devcontainer/post-script.sh
+++ .devcontainer/post-script.sh
@@ -2,6 +2,8 @@
 
 pip install -r ./requirements.txt
 
+chmod +x ./app/re-index-graphrag.sh
+
 echo ----------------------------------------------
 echo If you want help using the prompt library or list
 echo of available prompts, simply type:
